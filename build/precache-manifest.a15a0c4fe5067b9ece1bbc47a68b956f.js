67e48e2dd89831e1a07152e7a9f9ecd2edf02582
Ajay <majaysakthishankar@gmail.com>
2020-07-31 12:00:05 +0530

    first commit

---
+++ build/precache-manifest.a15a0c4fe5067b9ece1bbc47a68b956f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a65b9d38cc7649d6aa785ae41cb291d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "517b469649c117239c3d",
+    "url": "/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "aaa44d245f43500a4f75",
+    "url": "/static/js/2.942d19f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.942d19f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "517b469649c117239c3d",
+    "url": "/static/js/main.81d007d2.chunk.js"
+  },
+  {
+    "revision": "e90a02fb6b6a3d49eb98",
+    "url": "/static/js/runtime-main.70aaa4c8.js"
+  }
+]);
